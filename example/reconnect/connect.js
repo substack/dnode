bb1d787ddfeebc3bd8b2551365277a6979627c11
Kyle Mathews <mathews.kyle@gmail.com>
2015-08-12 12:03:16 -0700

    Add reconnect example
    Most production installs will need reconnection capability. This PR adds
    an example demonstrating how to use reconnect-core with dnode.
    
    Closes #154

---
+++ example/reconnect/connect.js
@@ -0,0 +1,29 @@
+var dnode = require('../../');
+var net = require('net');
+var inject = require('reconnect-core');
+
+var reconnect = inject(function() {
+  return net.connect.apply(null, arguments);
+  // End this every second.
+});
+
+reconnect({}, function(net) {
+    net.on('error', function(err) {
+      console.log(err);
+    });
+
+    // Initialize dnode.
+    var d = dnode();
+    net.pipe(d).pipe(net);
+
+    d.on('remote', function (remote) {
+        console.log("Connected to dnode server");
+        remote.transform('beep', function (s) {
+            console.log('beep => ' + s);
+        });
+    });
+
+    setTimeout(function(){console.log('closing dnode'); d.end();}, 2000);
+})
+.on('reconnect', function(n) { console.log("\n\nreconnecting...\n"); })
+.connect(5004);
---
+++ example/reconnect/listen.js
@@ -0,0 +1,16 @@
+var dnode = require('../../');
+var net = require('net');
+
+var server = net.createServer(function (c) {
+    c.on('error', function(err) {
+      console.log(err);
+    });
+    var d = dnode({
+        transform : function (s, cb) {
+            cb(s.replace(/[aeiou]{2,}/, 'oo').toUpperCase());
+        }
+    });
+    c.pipe(d).pipe(c);
+});
+
+server.listen(5004);
